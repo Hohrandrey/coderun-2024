990c03b8f794a413b03846e7d8b749d0f2845a94
Hohrandrey <EnderBro_2005@mail.ru>
2024-12-02 10:06:46 +0300

    1

--- 2.The_cheapest_way.py
+++ 2.The_cheapest_way.py
@@ -1,2 +1,23 @@
+import sys
+
+# Ввод N и M
+print("Введите N и M через пробел:")
 N, M = map(int, input().split())
+
+
+# Создание словаря для хранения данных
+data = {}
+
+# Чтение данных
 for i in range(N):
+    print(f"Введите строку {i + 1}:")
+    value = input()  # Читаем одну строку
+    data[f'var_{i}'] = value
+    
+for key in data.items():
+    for b in range(len(key)):
+        print(key[b])
+# Вывод результатов
+print("\nРезультирующий словарь:")
+print(data)
+print(f"\nN: {N}, M: {M}")
